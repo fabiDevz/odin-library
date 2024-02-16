e0d5c6f355ad08f954242d9e40ff569206ad6a7d
Fabian <70041644+FM9723@users.noreply.github.com>
2024-02-16 14:15:25 -0300

    refactorizado a clases

--- script.js
+++ script.js
@@ -10,51 +10,55 @@ const inputEstado = document.getElementById('input-checkbox');
 /* Elementos HTML */
 const tablaLibros = document.getElementById('books-table');
 
-/* Objetos */
-function Libro(titulo, autor, paginas, estado){
-    this.titulo = titulo;
-    this.autor = autor;
-    this.paginas = paginas;
-    this.estado = estado;
-    this.info =  function () {
-        console.log(this.titulo + ' cuyo autor es '+this.autor + ' con '+this.paginas+' páginas ')
-    } ;
-}
+/* Clases */
+class Libro {
 
+    constructor(titulo, autor, paginas, estado) {
+        this._titulo = titulo;
+        this._autor = autor;
+        this._paginas = paginas;
+        this._estado = estado;
+    }
+
+    info() {
+        console.log(this._titulo + ' cuyo autor es ' + this._autor + ' con ' + this._paginas + ' páginas ')
+    }
+}
 /* Eventos */
 
-formulario.addEventListener('submit',(e)=>{
+formulario.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    const libro = new Libro(
-        inputTitulo.value,
+
+    let libro = new Libro(
+        inputTitulo.value + "",
         inputAutor.value,
         inputPaginas.value,
         inputEstado.checked
     );
-    
+    console.log("valor input " + inputTitulo.value);
+
     agregarLibro(libro);
     formulario.reset();
-    
+
+    console.log(listaLibros);
+
 })
 
 /* Funciones */
 
-function agregarLibro(libroNuevo)
-{
+function agregarLibro(libroNuevo) {
     listaLibros.push(libroNuevo);
     mostrarLibros();
 }
 
-function mostrarLibros()
-{
+function mostrarLibros() {
     limpiarHTML();
 
     listaLibros.forEach(libro => {
-        let {titulo, autor, paginas, estado} = libro;
         
+
         var tbody = tablaLibros.getElementsByTagName('tbody')[0];
-        var fila = tbody.insertRow(); 
+        var fila = tbody.insertRow();
 
         celdaTitulo = fila.insertCell(0);
         celdaAutor = fila.insertCell(1);
@@ -62,32 +66,31 @@ function mostrarLibros()
         celdaEstado = fila.insertCell(3);
         celdaEliminar = fila.insertCell(4);
 
-        celdaTitulo.innerHTML = titulo;
-        celdaAutor.innerHTML = autor;
-        celdaPaginas.innerHTML = paginas;
+        celdaTitulo.innerHTML = libro._titulo;
+        celdaAutor.innerHTML = libro._autor;
+        celdaPaginas.innerHTML = libro._paginas;
 
         const btnCheck = document.createElement('button');
         btnCheck.onclick = (event) => cambioEstado(event);
-        btnCheck.textContent = estado ? "Si" : "No";
-        btnCheck.style.backgroundColor = estado ? 'green' : 'blue';
+        btnCheck.textContent = libro._estado ? "Si" : "No";
+        btnCheck.style.backgroundColor = libro._estado? 'green' : 'blue';
         btnCheck.classList.add('btn-read-status');
         celdaEstado.appendChild(btnCheck);
 
         const btnEliminar = document.createElement('button');
-        btnEliminar.onclick = () => eliminarLibro(titulo,autor);
+        btnEliminar.onclick = () => eliminarLibro(libro._titulo, libro._autor);
         btnEliminar.textContent = 'Eliminar';
         btnEliminar.classList.add('btn-delete-book');
         celdaEliminar.appendChild(btnEliminar);
-        
+
     });
 
 }
 
-function limpiarHTML(){
+function limpiarHTML() {
 
     var tbody = tablaLibros.getElementsByTagName('tbody')[0];
-    while(tbody.firstChild)
-    {
+    while (tbody.firstChild) {
         tbody.removeChild(tbody.firstChild);
     }
 }
@@ -99,9 +102,8 @@ function cambioEstado(event) {
     btn.style.backgroundColor = nuevoEstado ? 'green' : 'blue'; // Cambiamos el color de fondo del botón
 }
 
-function eliminarLibro(titulo, autor)
-{
-    listaLibros = listaLibros.filter( libro => libro.titulo != titulo || libro.autor != autor);
+function eliminarLibro(titulo, autor) {
+    listaLibros = listaLibros.filter(libro => libro._titulo != titulo || libro._autor != autor);
     mostrarLibros();
 }
 
